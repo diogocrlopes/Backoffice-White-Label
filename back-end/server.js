f2a2c78e58de062d4631e21d2bfa9810eb9fb06f
Diôgo Cavalcante Rodrigues Lopes <diogocrlopes@usp.br>
2021-12-27 13:58:47 -0300

    start of the project

---
+++ back-end/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+	, app = express()
+    , multer = require('multer')
+    , path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        
+        cb(null, 'arquivos/');
+    },
+    filename: function(req,file,cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+const type = upload.single('file');
+//app.use(express.static('public'));
+
+app.get('/post', (req, res) => res.send('ok'))
+
+app.post('/arquivo', type ,
+    (req,res) => res.send('<h2>Upload Realizado com sucesso</h2>'))
+
+app.listen(3000, () => console.log('App na porta 3000'));
